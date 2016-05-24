48cc2f1b8c2ca025f9f302c0e4c20a18e7c087f5
Franci <franci@Francis-MacBook-Air.local>
2016-05-23 20:05:12 -0400

    added some js

---
+++ javascript.js
@@ -0,0 +1,14 @@
+var delay=2000; 
+
+setTimeout(function() {
+
+	var arr = ["hello", "it's me", "I was wondering if after all these years you'd like to meet", "To go over everything","They say that time's supposed to heal ya", "But I ain't done much healing", "There's such a difference between us....And a million miles", "HELLO FROM THE OTHERSIDE!!"];
+
+	for (var i = 0; i < 8; i++){
+		var x = alert(arr[i]);
+	}
+
+}, delay);
+
+
+
